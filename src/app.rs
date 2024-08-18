fce3dd2f16b95e16ae69dcfd9f091db5e01108b1
Klaus Nielsen <hyperchaotic@gmail.com>
2024-08-18 15:14:13 -0700

    Polish color picker and popup dialog.

--- src/app.rs
+++ src/app.rs
@@ -129,6 +129,7 @@ pub struct Minimon {
     /// this counter can be set higher and controls refresh/update rate.
     /// Refreshes machine stats when reaching 0 and is reset to configured rate.
     tick_timer: u64,
+    information: String,
 }
 
 #[derive(Debug, Clone)]
@@ -167,7 +168,35 @@ impl cosmic::Application for Minimon {
     fn init(core: Core, _flags: Self::Flags) -> (Self, Command<Self::Message>) {
         let mut system = System::new();
         system.refresh_memory();
-        let mem_physical = system.total_memory(); // To GB
+        system.refresh_cpu_all();
+        let mem_physical = system.total_memory();
+
+        let mut info = String::new();
+
+        info.push_str(&format!(
+            "    {} {}\n",
+            System::name().unwrap_or_default(),
+            System::os_version().unwrap_or_default(),
+        ));
+
+        info.push_str(&format!(
+            "        Kernel version:   {}\n",
+            System::kernel_version().unwrap_or_default()
+        ));
+
+        let mem = system
+            .total_memory()
+            .to_string()
+            .as_bytes()
+            .rchunks(3)
+            .rev()
+            .map(std::str::from_utf8)
+            .collect::<Result<Vec<&str>, _>>()
+            .unwrap()
+            .join(",");
+
+        info.push_str(&format!("        Total memory: {} bytes\n", mem));
+        info.push_str(&format!("        Number of CPU cores: {}\n ", system.cpus().len()));
 
         let app = Minimon {
             core,
@@ -180,6 +209,7 @@ impl cosmic::Application for Minimon {
             colorpicker: ColorPicker::default(),
             config: MinimonConfig::default(),
             tick_timer: 1000,
+            information: info,
         };
 
         (app, Command::none())
@@ -393,6 +423,11 @@ impl cosmic::Application for Minimon {
             let content_list = widget::list_column()
                 .padding(5)
                 .spacing(0)
+                .add(
+                    widget::text::body(self.information.clone())
+                        .width(Length::Fill)
+                        .horizontal_alignment(Horizontal::Left),
+                )
                 .add(settings::item(
                     fl!("text-only"),
                     widget::toggler(None, self.config.text_only, |value| {
